b1df3e273909b78880e08b7add017520bbe87e21
o0t1ng0o <453381299@qq.com>
2016-04-14 15:53:36 +0800

    v

--- Stage_2/Task 16/js/task.js
+++ Stage_2/Task 16/js/task.js
@@ -43,9 +43,21 @@ function delBtnHandle() {
 }
 
 function init() {
-
+    var btn=document.getElementById("add-btn");
+   
+    btn.addEventListener("click",function(){
+       
+        addBtnHandler();
+   
+    },false);
   // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-
+    var btn=document.getElementById("del-btn");
+   
+    btn.addEventListener("click",function(){
+       
+        delBtnHandler();
+   
+    },false);
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
 
 }
